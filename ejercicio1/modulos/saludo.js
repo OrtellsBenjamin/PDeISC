9b379e49eb0b9695703549aaa9029a54717e4dcb
OrtellsBenjamin <benjaminortells26@gmail.com>
2025-04-17 14:29:40 -0300

    .

---
+++ ejercicio1/modulos/saludo.js
@@ -0,0 +1,8 @@
+// Este módulo va  a exportar una función que saluda a una persona por su nombre
+
+function saludar(nombre) {
+    return `¡Buenas Tardes, ${nombre}!`;
+  }
+  
+  module.exports = saludar;
+  
