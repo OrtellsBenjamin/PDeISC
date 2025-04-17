672e40be6cedfd39f534832af871bfcf40b642f7
Ortells Benjamin <130561238+OrtellsBenjamin@users.noreply.github.com>
2025-04-17 14:44:23 -0300

    .

--- ejercicio1/modulos/tiempo.js
+++
@@ -1,9 +0,0 @@
-// Este módulo va a exportar una función que devuelve la hora actual en formato de cadena.
-
-function obtenerHoraActual() {
-    const ahora = new Date();
-    return ahora.toLocaleTimeString();
-  }
-  
-  module.exports = obtenerHoraActual;
-  
