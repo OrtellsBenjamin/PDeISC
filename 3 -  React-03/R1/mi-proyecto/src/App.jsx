1a1d18a856cb2c3658a8389f4cde7552d912dcbf
OrtellsBenjamin <benjaminortells26@gmail.com>
2025-08-14 16:12:05 -0300

    .

---
+++ 3 -  React-03/R1/mi-proyecto/src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import HolaMundo from "./Componentes/HolaMundo/HolaMundo";
+import TarjetaPresentacion from "./Componentes/TarjetaPresentacion/TarjetaPresentacion";
+import Contador from "./Componentes/Contador/Contador";
+import ListaTareas from "./Componentes/ListaTareas/ListaTareas";
+import Formulario from "./Componentes/Formulario/Formulario";
+
+export default function App() {
+  return (
+    <div className="container py-4">
+      <div className="mb-4">
+        <HolaMundo />
+      </div>
+      <div className="mb-4">
+        <TarjetaPresentacion
+          nombre="Juan"
+          apellido="Pérez"
+          profesion="Desarrollador Web"
+          imagen="https://hireline.io/blog/wp-content/uploads/2022/07/habilidades-de-un-programador-1200x900.jpg"
+        />
+      </div>
+      <div className="mb-4">
+        <Contador />
+      </div>
+      <div className="mb-4">
+        <ListaTareas />
+      </div>
+      <div className="mb-4">
+        <Formulario />
+      </div>
+    </div>
+  );
+}
---
+++ 3 -  React-03/R1/mi-proyecto/src/Componentes/HolaMundo/HolaMundo.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./HolaMundo.css";
+
+export default function HolaMundo() {
+  return <h1 className="titulo">¡Hola, mundo!</h1>;
+}
