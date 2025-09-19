f55e431b9df3ba1ef2daa13066ecb1060b453851
OrtellsBenjamin <benjaminortells26@gmail.com>
2025-09-19 01:10:31 -0300

    .

--- 3 -  React-03/R4/frontend/src/lib/supabaseClient.js
+++ 3 -  React-03/R4/frontend/src/lib/supabaseClient.js
@@ -1,7 +1,8 @@
-// src/lib/supabaseClient.js
+
 import { createClient } from "@supabase/supabase-js";
 
-// Usar las variables de entorno de Vercel
+console.log(import.meta.env.VITE_SUPABASE_URL)
+
 const supabaseUrl = import.meta.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
